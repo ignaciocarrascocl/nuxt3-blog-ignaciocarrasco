2e1c39c34b4385ed02884c484ced0888bffb6d42
ignaciocarrascocl <icarrasco@caput.cl>
2025-03-17 21:18:47 -0300

    back

--- app.vue
+++ app.vue
@@ -1,25 +1,11 @@
 <template>
-  <div>
-    <header>
-      <h1><NuxtLink to="/">My Simple Blog</NuxtLink></h1>
-    </header>
-    <slot />
-    <footer>
-      <p>&copy; {{ new Date().getFullYear() }} My Simple Blog</p>
-    </footer>
-  </div>
+	<div
+		id="site-wrapper"
+	>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
 </template>
 
-<style lang="scss">
-header, footer {
-  text-align: center;
-  padding: 1rem;
-  background: #f8f8f8;
-  border-bottom: 1px solid #ddd;
-}
 
-footer {
-  margin-top: 2rem;
-  border-top: 1px solid #ddd;
-}
-</style>
